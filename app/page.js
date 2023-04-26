c25fcbd8b87651a3dd543071d26961191a826efb
Ohjinseo2022 <dhwlstj@gmail.com>
2023-04-26 23:35:27 +0900

    NextJS 라우팅 방법

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 export default function Home() {
   let name = "오진서";
   let age = 32;
-  let link = "https://google.com";
   return (
     <div>
-      <h4 className="title" style={{ color: "blue", fontSize: "30px" }}>
+      <h4 className="title" style={{ fontSize: "30px" }}>
         애플후레시
       </h4>
       <p className="title-sub">by dev {`${name} ${age}세`}</p>
