916b8b32f7b058d97b09e2b309654637faa93df3
Ohjinseo2022 <dhwlstj@gmail.com>
2023-04-30 17:51:14 +0900

    server component 와 client component의 차이점

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,7 +1,37 @@
+"use client";
+
 export default function Cart() {
   return (
     <div>
-      <h1>장바구니입니다</h1>
+      <h4 className="title">Cart</h4>
+      {[0, 1, 2, 3, 4, 5].map((e) => {
+        return <CartItem />;
+      })}
     </div>
   );
 }
+
+function CartItem() {
+  return (
+    <div className="cart-item">
+      <p>상품명</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  );
+}
+
+/* next.js 컴포넌트는 종류가 2개임
+1. server component
+아무위치에서나 function 사용시 서버 컴포넌트
+서버컴포넌트안에서는 자바스크립트 기능을 집어넣을수가 없음 -> ex)onClick 이벤트등
+단 로딩이 빠름 
+--> 큰페이지에서는 server component 사용을 권장하고 작은단위 컴포넌트(JS기능이 필요한)는 clinet component 사용을 권장함 
+2. client component  
+js파일 맨위에 'use client' 문구 작성후 사용시 
+클라이언트 컴포넌트
+자바스크립트 이벤트 문법 사용가능
+로딩이 느림 1(자바스크립트많이 필요함)
+로딩이 느림 2 (hydration 필요)
+
+*/
--- app/list/page.js
+++ app/list/page.js
@@ -1,3 +1,9 @@
+// import Image from "next/image"; //next에서 지원하는 Image 태그를 사용하면
+// //무조건 이미지를 import해와서 가져다 써야함 해당태그는 이미지를 자동으로 최적화 사이즈로 조정해줌
+// import food0 from "@/public/food0.png";
+// import food1 from "@/public/food1.png";
+// import food2 from "@/public/food2.png";
+
 export default function List() {
   let product = [
     { prd: "Tomatoes", price: 4000 },
@@ -10,7 +16,8 @@ export default function List() {
       <h3 className="title">상품목록</h3>
       {product.map((e, idx) => {
         return (
-          <div className="food">
+          <div className="food" key={`prd=${idx}`}>
+            <img src={`/food${idx}.png`} className="food-img" />
             <h4>{`${idx + 1}. ${e.prd} ${e.price}`}</h4>
           </div>
         );
